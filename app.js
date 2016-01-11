6709170bf584d048dfa6d29a18b8b13901e2ca13
Jeff Sallans <jeffsallans@gmail.com>
2016-01-10 22:10:21 -0500

    Working state: fixed controller syntax

--- app.js
+++ app.js
@@ -63,24 +63,24 @@ angular.module('functionJunctionApp', ['ngRoute', 'ui.bootstrap'])
 
 
 	}])
-.controller('HomeCtrl', ['$scope'], function($scope) {
-	$scope.myInterval = 5000;
-	$scope.noWrapSlides = false;
-	var slides = $scope.slides = [];
-	var currIndex = 0;
+	.controller('HomeCtrl', ['$scope', function($scope) {
+		$scope.myInterval = 5000;
+		$scope.noWrapSlides = false;
+		var slides = $scope.slides = [];
+		var currIndex = 0;
 
-	$scope.addSlide = function() {
-		var newWidth = 600 + slides.length + 1;
-		slides.push({
-			image: '//placekitten.com/' + newWidth + '/300',
-			text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-			['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4],
-			id: currIndex++
-		});
-	};
+		$scope.addSlide = function() {
+			var newWidth = 600 + slides.length + 1;
+			slides.push({
+				image: '//placekitten.com/' + newWidth + '/300',
+				text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+				['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4],
+				id: currIndex++
+			});
+		};
 
-	//Create initial slides
-	for (var i = 0; i < 4; i++) {
-		$scope.addSlide();
-	}
-})
+		//Create initial slides
+		for (var i = 0; i < 4; i++) {
+			$scope.addSlide();
+		}
+	}]);
